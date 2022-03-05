823f986abd025055dc63c3e2efe938c190902316
Cody <48037883+uncoded0123@users.noreply.github.com>
2022-03-05 14:04:48 -0600

    added function call, removed unused module

--- led_strip_on_off.py
+++ led_strip_on_off.py
@@ -1,13 +1,12 @@
 from machine import reset, Pin
-from time import sleep, sleep_ms
-import neopixel, led_on_off
+from time import sleep_ms
+import neopixel
 
 
 def leds(loops=2):
     n = 300
     npx = neopixel.NeoPixel(Pin(2), n)
     color = {'red': (255,0,0), 'green': (0,255,0), 'blue':(0,0,255)}
-    
     for i in range(loops):
         for ind, col in enumerate(color):
             for pix in range(0,160,5):
@@ -17,3 +16,4 @@ def leds(loops=2):
                 npx[pix] = (0,0,0)
                 npx.write()
     sleep_ms(1000)
+leds()
