9a7c547f815e4ccb671266909d8e7373f9834ff0
Cody <48037883+uncoded0123@users.noreply.github.com>
2022-03-05 13:40:06 -0600

    variable name change

--- led_strip_on_off.py
+++ led_strip_on_off.py
@@ -5,15 +5,15 @@ import neopixel, led_on_off
 
 def leds(loops=2):
     n = 300
-    np = neopixel.NeoPixel(Pin(2), n)
+    npx = neopixel.NeoPixel(Pin(2), n)
     color = {'red': (255,0,0), 'green': (0,255,0), 'blue':(0,0,255)}
     
     for i in range(loops):
         for ind, col in enumerate(color):
             for pix in range(0,160,5):
-                np[pix] = color[col]
-                np.write()
+                npx[pix] = color[col]
+                npx.write()
             for pix in range(0,160,5):
-                np[pix] = (0,0,0)
-                np.write()
+                npx[pix] = (0,0,0)
+                npx.write()
     sleep_ms(1000)
